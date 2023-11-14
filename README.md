1c6fc64943133d0c0b529b7c3cbd15e1151cd477
Checksum <me@checksum.space>
2023-11-14 15:44:22 -0600

    feat: it's propaganda time

---
+++ README.md
@@ -0,0 +1,2 @@
+# PropagandaLabs
+Replaces the lab sound with Mao Zedong's red sun in the sky, forked from LouderLabDansen by NyanCation
---
+++ prototypes/sound.lua
@@ -0,0 +1,10 @@
+-- The Lab
+data.raw["lab"]["lab"].working_sound = {
+    sound = {
+        filename = "__Propaganda Labs__/sounds/propaganda.ogg",
+        volume = 2.5
+    },
+    max_sounds_per_type = 1,
+    idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
+    apparent_volume = 2.5,
+}
